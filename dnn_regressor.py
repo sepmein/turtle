10a6c31fc2b57b5c39b74b400cf76454744d5682
Spencer Zhang <sepmein@gmail.com>
2017-07-06 14:06:25 +0800

    redefined data generation function

---
+++ dnn_regressor.py
@@ -0,0 +1,146 @@
+"""
+Read data from csv
+"""
+# TODO: inverse-scale data, get the params of the scaler
+# TODO: split data in to three parts: training, validation and test
+# TODO: improve logging and tensorboard
+# TODO: universal input fn
+# TODO: predict function
+# TODO: define fn to import trained model
+# FIXME: Generation data error, should not generate data after random selection
+
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+from sklearn import preprocessing
+
+# Setting logging verbosity
+tf.logging.set_verbosity(tf.logging.INFO)
+
+# Define target label
+TARGET_LABEL = ['MKPRU']
+
+# Define raw labels, from which function will generate feature labels
+RAW_LABELS = [
+    'DIFF', 'TRFEE', 'MKTCP', 'TOTBC', 'MWNUS', 'MWNTD', 'MWTRV', 'AVBLS',
+    'BLCHS', 'ATRCT', 'MIREV', 'HRATE', 'CPTRA', 'CPTRV', 'TRVOU', 'TOUTV',
+    'ETRVU', 'ETRAV', 'NTRBL', 'NADDU', 'NTREP', 'NTRAT', 'NTRAN'
+]
+
+
+def gen_feature_labels(labels, days):
+    """
+        generate data by days back
+        using "days" back data to predict BTC_USD value
+    """
+    gen_labels = []
+    for label in labels:
+        for j in range(days):
+            gen_labels.append(label + '_' + str(j + 1))
+    return gen_labels
+
+# Generate feature labels
+GEN_FEATURE_LABELS = gen_feature_labels(RAW_LABELS, 10)
+
+# read data from csv file
+gen_feature_data_training = pd.read_csv('gen_feature_data_training.csv').loc[:,'DIFF_1':]
+gen_feature_data_cv = pd.read_csv('gen_feature_data_cv.csv').loc[:,'DIFF_1':]
+gen_feature_data_test = pd.read_csv('gen_feature_data_test.csv').loc[:,'DIFF_1':]
+gen_target_data_training = pd.read_csv('gen_target_data_training.csv').loc[:,'MKPRU']
+gen_target_data_cv = pd.read_csv('gen_target_data_cv.csv').loc[:,'MKPRU']
+gen_target_data_test = pd.read_csv('gen_target_data_test.csv').loc[:,'MKPRU']
+
+# build tensorflow input layers
+FEATURES = []
+for gen_feature_label in GEN_FEATURE_LABELS:
+    FEATURES.append(tf.contrib.layers.real_valued_column(gen_feature_label))
+
+# Data scaler
+SCALER = preprocessing.StandardScaler()
+FEATURE_SCALER = SCALER.fit(gen_feature_data_training)
+TARGET_SCALER = SCALER.fit(gen_target_data_training)
+
+
+def input_fn(feature, target):
+    """
+        Build Input function
+        Using tf.constant to build feature and target value
+    """
+    features_tf = {
+        k: tf.constant(FEATURE_SCALER.fit_transform(feature[k]))
+        for k in GEN_FEATURE_LABELS
+    }
+    target_tf = tf.constant(TARGET_SCALER.fit_transform(target))
+    return features_tf, target_tf
+
+
+def input_fn_train():
+    """
+        Build Input function
+        Using tf.constant to build feature and target value
+    """
+    features_tf, target_tf = input_fn(
+        feature=gen_feature_data_training,
+        target=gen_target_data_training
+    )
+    return features_tf, target_tf
+
+
+def input_fn_eval():
+    """
+        input_fn_eval, a function to build eval function
+    """
+    features_tf, target_tf = input_fn(
+        feature=gen_feature_data_cv,
+        target=gen_target_data_cv
+    )
+    return features_tf, target_tf
+
+
+def input_fn_test():
+    """
+        input_fn_test, a function to build test function
+    """
+    features_tf, target_tf = input_fn(
+        feature=gen_feature_data_test,
+        target=gen_target_data_test
+    )
+    return features_tf, target_tf
+
+# Added a layer of validation monitor
+validation_monitor = tf.contrib.learn.monitors.ValidationMonitor(
+    input_fn=input_fn_eval,
+    every_n_steps=100,
+    eval_steps=1
+)
+
+# Building models
+# tf dnn regressor is used
+# TODO: consider RNN model?
+ESTIMATOR = tf.contrib.learn.DNNRegressor(
+    feature_columns=FEATURES,
+    hidden_units=[256, 128, 64],
+    config=tf.contrib.learn.RunConfig(save_checkpoints_secs=30),
+    dropout=0.01
+    # optimizer=tf.train.ProximalAdagradOptimizer(
+    #    learning_rate=0.05, l1_regularization_strength=0.1)
+)
+
+# Fit train model
+ESTIMATOR.fit(
+    input_fn=input_fn_train,
+    monitors=[validation_monitor],
+    steps=100000
+)
+
+# Cross validate data
+ESTIMATOR.evaluate(
+    input_fn=input_fn_eval,
+    steps=1
+)
+
+# Test and predict
+ESTIMATOR.evaluate(
+    input_fn=input_fn_test,
+    steps=1
+)
--- generate_data.py
+++ generate_data.py
@@ -3,21 +3,99 @@
 """
 import numpy as np
 import pandas as pd
+
+# read raw data
 RAW_DATA = pd.read_csv('cleaned_data.csv')
+
+# Define raw labels, from which function will generate feature labels
+RAW_LABELS = [
+    'DIFF', 'TRFEE', 'MKTCP', 'TOTBC', 'MWNUS', 'MWNTD', 'MWTRV', 'AVBLS',
+    'BLCHS', 'ATRCT', 'MIREV', 'HRATE', 'CPTRA', 'CPTRV', 'TRVOU', 'TOUTV',
+    'ETRVU', 'ETRAV', 'NTRBL', 'NADDU', 'NTREP', 'NTRAT', 'NTRAN'
+]
+
+# Define target label
+TARGET_LABEL = ['MKPRU']
+
 # Define machine learning ratio
 TRAINING_RATIO = 0.6
 CROSS_VALIDATION_RATIO = 0.3
 TEST_RATIO = 0.1
 
-# Generate three parts of data
-MASK = np.random.rand(len(RAW_DATA))
+# Define how much rows should be skipped
+# Because at the initial year of bitcoin, there weren't any $-BTC data.
+# So it should be skipped
+STARTS_AT = 200
+
+# Convert data string to pandas datatime format
+RAW_DATA['Date'] = pd.to_datetime(RAW_DATA['Date'])
+
+
+
+def gen_days_back(data, labels, days, starts_at):
+    """
+        generate data by days back
+        using "days" back data to predict BTC_USD value
+    """
+    gen_labels = []
+    gen_data = []
+    for label in labels:
+        for j in range(days):
+            gen_labels.append(label + '_' + str(j + 1))
+    for k in range(starts_at, data.shape[0]):
+        days_back_data = data[k - days:k]
+        selected_day_back_data = days_back_data.loc[:, 'DIFF':'NTRAN']
+        selected_day_back_data_np = selected_day_back_data.values
+        reshaped = np.reshape(selected_day_back_data_np.T,
+                              (1, len(labels) * days))
+        gen_data.append(reshaped)
+    stacked = np.vstack(row for row in gen_data)
+    dataframe = pd.DataFrame(data=stacked, columns=gen_labels)
+    return dataframe, gen_labels
+
+# Generate feature data
+gen_feature_data, gen_labels = gen_days_back(
+    data=RAW_DATA,
+    labels=RAW_LABELS,
+    days=10,
+    starts_at=STARTS_AT
+)
+
+# Generate target data fn
+
+
+def gen_target_data(data, label, starts_at):
+    """
+        generate raw target data
+    """
+    result = data[starts_at:][label]
+    return result
+
+# Generate target data
+target_data = gen_target_data(
+    data=RAW_DATA,
+    label=TARGET_LABEL,
+    starts_at=STARTS_AT
+)
+
+# Define random mask
+MASK = np.random.rand(len(gen_feature_data))
 MASK_TRAIN = MASK <= 0.6
 MASK_CROSS_VALIDATION = (MASK > 0.6) & (MASK < 0.9)
 MASK_TEST = MASK >= 0.9
-RAW_DATA_TRAIN = RAW_DATA[MASK_TRAIN]
-RAW_DATA_CROSS_VALIDATION = RAW_DATA[MASK_CROSS_VALIDATION]
-RAW_DATA_TEST = RAW_DATA[MASK_TEST]
+
+# Generate three kinds of data
+gen_feature_data_training = gen_feature_data[MASK_TRAIN]
+gen_feature_data_cv = gen_feature_data[MASK_CROSS_VALIDATION]
+gen_feature_data_test = gen_feature_data[MASK_TEST]
+gen_target_data_training = target_data[MASK_TRAIN]
+gen_target_data_cv = target_data[MASK_CROSS_VALIDATION]
+gen_target_data_test = target_data[MASK_TEST]
+
 # save three parts of data
-RAW_DATA_TRAIN.to_csv('raw_data_train.csv')
-RAW_DATA_CROSS_VALIDATION.to_csv('raw_data_cross_validation.csv')
-RAW_DATA_TEST.to_csv('raw_data_test.csv')
+gen_feature_data_training.to_csv('gen_feature_data_training.csv')
+gen_target_data_training.to_csv('gen_target_data_training.csv')
+gen_feature_data_cv.to_csv('gen_feature_data_cv.csv')
+gen_target_data_cv.to_csv('gen_target_data_cv.csv')
+gen_feature_data_test.to_csv('gen_feature_data_test.csv')
+gen_target_data_test.to_csv('gen_target_data_test.csv')

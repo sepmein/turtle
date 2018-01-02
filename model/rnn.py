05298b33dd5b65ff467a425ff1c1d97a6be8ec6c
sepmein <sepmein@gmail.com>
2018-01-02 17:08:24 +0800

    build rnn using twone and oyou

--- model/rnn.py
+++ model/rnn.py
@@ -1,40 +1,42 @@
-# hidden_size = 100
-# batch_size = 64
-# feature_size =
-#
-# rnn_cell = tf.nn.rnn_cell.BasicLSTMCell(hidden_size)
-# initial_state = rnn_cell.zero_state(batch_size, dtype=tf.float32)
-#
-# input_data = tf.placeholder(shape=(batch_size, max_length, feature_size), dtype=tf.float32)
-#
-# outputs, state = tf.nn.dynamic_rnn(rnn_cell, input_data, initial_state=initial_state, dtype=tf.float32)
-#
-# loss = tf.reduce_mean(tf.abs(outputs - y))
-#
-# optimizer = tf.train.AdamOptimizer().minimize(loss)
-#
-# session = tf.Session()
-# session.run(tf.global_variables_initializer())
-#
-# for i in range(100000):
-#     l, _ = session.run(
-#         [loss, optimizer],
-#         feed_dict={
-#             input_data: x
-#         }
-#     )
-#     if i % 100 == 0:
-#         print(l)
-#
-from twone import Container
+import tensorflow as tf
+from twone import RNNContainer as Container
 
 from config import feature_labels, target_label
 from data_fetcher import scrap
 
+####################################################################
+# scrap data
+####################################################################
 fetched_raw_df = scrap()
 
+####################################################################
+# process data using twone
+####################################################################
 container = Container(data_frame=fetched_raw_df)
 # todo: change interpolate api to the most recent version
-container.interpolate(container.data)
-container.set_feature_tags(feature_tags=feature_labels)
-container.set_target_tags(target_tags=target_label)
+container.interpolate() \
+    .normalize() \
+    .set_feature_tags(feature_tags=feature_labels) \
+    .set_target_tags(target_tags=target_label)
+container.gen_batch(
+    batch=5,
+    time_steps=20
+)
+num_features = container._num_features
+num_targets = container._num_targets
+
+#####################################################################
+# build tensorflow graph
+#####################################################################
+features = tf.placeholder(dtype=tf.float32,
+                          shape=[None, None, num_features],
+                          name='features')
+targets = tf.placeholder(dtype=tf.float32,
+                         shape=[None, None, num_targets],
+                         name='targets')
+cell = tf.contrib.rnn.LSTMCell(10, state_is_tuple=True)
+rnn_outputs, final_state = tf.nn.dynamic_rnn(cell=cell,
+                                             inputs=features)
+#####################################################################
+# continue with oyou
+#####################################################################
